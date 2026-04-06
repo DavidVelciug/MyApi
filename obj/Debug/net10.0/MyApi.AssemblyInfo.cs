39e956b515d1b0f53941cae7ac72d053ce6bb32b
DmytroPazhytniev <pajitnev.dmitro@gmail.com>
2026-04-06 18:19:31 +0300

    Обновлённая версия Бэкенда

--- obj/Debug/net10.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31286e0e454539ec75c388f438d1ed24e2eab067")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b9cb0adf24fcbb0e346062126c1550b643bacd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
